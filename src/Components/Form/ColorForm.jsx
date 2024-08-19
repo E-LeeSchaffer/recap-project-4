f8c81fd8f80ba739969c85d8717159e5f6d23eb6
Eun-Rae Lee-Schaffer <eunraelee.erl@gmail.com>
2024-08-19 09:58:36 +0200

    implement uid

--- src/Components/Form/ColorForm.jsx
+++ src/Components/Form/ColorForm.jsx
@@ -1,3 +1,6 @@
+import "./ColorForm.css";
+import { uid } from "uid";
+
 export default function ColorForm({
   onAddColor,
   initialData = { role: "some color", hex: "#123456", contrastText: "#ffffff" },
@@ -6,28 +9,39 @@ export default function ColorForm({
     event.preventDefault();
     const formData = new FormData(event.target);
     const data = Object.fromEntries(formData);
-    onAddColor(data);
+    const newColor = { id: uid(), ...data };
+    onAddColor(newColor);
   }
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form className="input-form" onSubmit={handleSubmit}>
       <label htmlFor="role">Role</label>
       <input
+        className="input-field"
         type="text"
         id="role"
         name="role"
         defaultValue={initialData.role}
       />
       <label htmlFor="hex">Hex</label>
-      <input type="text" id="hex" name="hex" defaultValue={initialData.hex} />
+      <input
+        className="input-field"
+        type="text"
+        id="hex"
+        name="hex"
+        defaultValue={initialData.hex}
+      />
       <label htmlFor="contrastText">Contrast Text</label>
       <input
+        className="input-field"
         type="text"
         id="contrastText"
         name="contrastText"
         defaultValue={initialData.contrastText}
       />
-      <button type="submit">Add Color</button>
+      <button className="button" type="submit">
+        ADD COLOR
+      </button>
     </form>
   );
 }
