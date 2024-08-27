1a3a253b6c348cde9c7bcc9a45bda61faa3f35ae
Eun-Rae Lee-Schaffer <eunraelee.erl@gmail.com>
2024-08-27 13:33:41 +0200

    delete comment

--- src/lib/themes.js
+++ src/lib/themes.js
@@ -5,21 +5,21 @@ export const initialThemes = [
     id: "t1",
     name: "Default Theme",
     colors: [
-      initialColors[0], // c1: primary main
-      initialColors[1], // c2: primary dark
-      initialColors[2], // c3: primary light
-      initialColors[3], // c4: secondary main
-      initialColors[4], // c5: secondary dark
+      initialColors[0],
+      initialColors[1],
+      initialColors[2],
+      initialColors[3],
+      initialColors[4],
     ],
   },
   {
     id: "t2",
     name: "2nd Theme",
     colors: [
-      initialColors[5], // c6: secondary light
-      initialColors[6], // c7: background main
-      initialColors[7], // c8: background dark
-      initialColors[8], // c9: background light
+      initialColors[5],
+      initialColors[6],
+      initialColors[7],
+      initialColors[8],
     ],
   },
 ];
